be14a97edb2b41ff2ffd96edd05c43ab74c265d9
GeraldoSaroinsong <geraldosaroinsong5@gmail.com>
2024-11-05 14:46:18 +0700

    added links to some buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,13 @@ const Navbar = () => {
   return (
     <section className="p-2 md:p-6 bg-yellow-300">
       <div className="w-[90%] md:w-3/4 m-auto flex justify-between items-center">
-        <h1 className="text-base md:text-3xl font-semibold flex flex-col">
+        <a
+          href="https://geraldo-saroinsong.vercel.app"
+          className="text-base md:text-3xl font-semibold flex flex-col"
+        >
           <span className="bg-black text-yellow-300 w-full">Sar</span>
           <span className="underline underline-offset-4">Portfolio</span>
-        </h1>
+        </a>
         <h1 className="text-2xl md:text-6xl font-bold">#</h1>
         <a
           href="https://wa.me/0895341479858"
